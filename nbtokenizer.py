41d44fd407f78758f2b7a0efc2451df1e0480e0f
Lars G Johnsen <yoonsen@gmail.com>
2018-11-01 17:46:38 +0100

    Update nbtokenizer.py

--- nbtokenizer.py
+++ nbtokenizer.py
@@ -12,11 +12,9 @@ Tokenisator for ngramleser (evt. parsing).
 -------------------------------------------
 Lars G Johnsen, Nasjonalbiblioteket, juni 2014
 
+Tokenisatorens oppgave er å danne token eller ord fra en sekvens med tegn. I utgangspunktet fungerer skilletegn og mellomrom som ordgrenser, men det er unntak, se listen nedenfor. Skilletegn danner som oftest egne token, men spesielt punktum og komma brukes på flere måter, noe det må tas høyde for.
 
-
-Tokenisatorens oppgave er å skille ut token (ord) fra en sekvens med tegn. I utgangspunktet består ord av bokstaver skilt fra hverandre med skilletegn og mellomrom, men det er unntak. Formålet er å sikre at det som telles er meningsfylt, og at alle ord blir jevnt telt. For søk i ettertid er det også viktig at man tokeniserer enheter som man ønsker å lete opp. Skilletegn må være med som token og telles opp separat. Punktum og komma kan brukes på flere måter, noe det må tas høyde for.
-
-Noen ord (token) inneholder skilletegn, som forkortelser, tall, og ordene selv kan være bundet sammen med bindestrek:
+Noen ord (token) har bestanddeler i form av skilletegn, som forkortelser, tall, i tillegg kan ordene selv være bundet sammen med bindestrek:
 
 p-pille (bindestrek)
 3.3 (punktum i seksjonsnummerering)
@@ -28,19 +26,17 @@ co2-forurensning (bokstaver tall pluss bindestrek)
 17. (ordenstall som i 17. mai)
 P. A. Munch (punktum i initialer) 
 ... tre eller flere  punktum
-
 Når punktum følger tall vil tokenisatoren la punktum tilhøre tallet med mindre punktumet følges av mellomrom og stor bokstav.
 
-Punktum tilhører alle forkortelser som tar punktum uavhenging av kontekst. Den kan imidlertid gjøres følsom for påfølgende stor bokstav, men det er altså ikke gjort her. Punktum tillates inne i ord.
+Punktum tilhører alle forkortelser som tar punktum uavhenging av kontekst. Den kan imidlertid gjøres følsom for påfølgende stor bokstav, men det er altså ikke gjort her. Punktum tillates inne i ord og deler ikke opp ord med punktum i seg.
 
-Alle skilletegn ellers utgjør egne token, bortsett fra § som kan sekvensieres, så § og §§ vil være egne tokener; de benyttes en hel del i lovtekster.
+Alle skilletegn ellers utgjør egne token, bortsett fra § som kan sekvensieres, så § og §§ vil være egne tokener; de benyttes en hel del i lovtekster for entall og flertall.
 
-Tall skrevet med mellomrom blir ett token om de er på formen xx xxx, altså 1 eller 3 siffer etterfulgt av grupper på tre siffer skilt med ett mellomrom. Så 3 1995 vil være to tokener, mens 3 995 blir ett token, 4000 398 blir igjen to token.
+Tall skrevet med mellomrom blir ett token om de er på formen xx xxx, altså 1 eller 3 siffer etterfulgt av grupper på tre siffer skilt med ett mellomrom. Så 3 1995 vil være to tokener, mens 3 995 blir ett token, 4000 398 blir igjen to token. (Mulig det er endret)
 
 Tall som følger etter § (adskilt med maks ett mellomrom) vil aldri tiltrekke seg punktum.
 
 Øvrige tegn som ikke passer inn med mønstrene over behandles som separate token.
-
 
 
 =========================================================================================="""
