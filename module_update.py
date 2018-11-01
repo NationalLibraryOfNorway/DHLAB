fcefda19e8b00b75c132de101104551502854fd7
Lars G Johnsen <yoonsen@gmail.com>
2018-11-01 19:06:41 +0100

    Update module_update.py

--- module_update.py
+++ module_update.py
@@ -1,22 +1,26 @@
 import requests
+import os
 from IPython.display import HTML
 from urllib.parse import urlparse, urljoin
 
-def update(module=""):
+def update(module="", overwrite=False):
     """Fetch modules from Github and write them to folder"""
     nba = requests.get(
         "https://raw.githubusercontent.com/Yoonsen/Modules/master/{module}.py".format(module=module),
         headers={'Cache-Control': 'no-cache'}
         )
-    
+    filename = '{m}.py'.format(m=module)
     if nba.status_code == 200:
-        nba = nba.text
-        with open('{m}.py'.format(m=module),'w', encoding='UTF-8') as pyfile:
-            pyfile.write(nba)
-            pyfile.flush()
-        print("Updated file {module}.py".format(module=module))
+        if os.path.exists(filename) and not(overwrite):
+            print("file {f} exists - specify 'overwrite=True' as parameter to store downloaded {f} anyway".format(f=filename))
+        else:
+            nba = nba.text
+            with open(filename,'w', encoding='UTF-8') as pyfile:
+                pyfile.write(nba)
+                pyfile.flush()
+            print("Updated file {module}.py stored".format(module=module))
     else:
-        print("An error occured ", module, nba.status_code)
+        print("An error occured during download ", module, nba.status_code)
     return
 
 def css(url = "https://raw.githubusercontent.com/Yoonsen/Modules/master/css_style_sheets/nb_notebook.css"):
