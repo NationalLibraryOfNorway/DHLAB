f70ea46ec47ca3e6bf820582230474017c687737
Lars G Johnsen <yoonsen@gmail.com>
2018-11-20 13:09:12 +0100

    Update module_update.py

--- module_update.py
+++ module_update.py
@@ -2,8 +2,50 @@ import requests
 import os
 from IPython.display import HTML, Markdown, display
 from urllib.parse import urlparse, urljoin
+import ipywidgets as widgets
 
 
+
+
+def code_toggle():
+    """Code taken from Erick Shepherd at 
+    stackoverflow https://stackoverflow.com/questions/27934885/how-to-hide-code-from-cells-in-ipython-notebook-visualized-with-nbviewer"""
+    javascript_functions = {False: "hide()", True: "show()"}
+    button_descriptions  = {False: "Show code", True: "Hide code"}
+    def toggle_code(state):
+
+        """
+        Toggles the JavaScript show()/hide() function on the div.input element.
+        """
+
+        output_string = "<script>$(\"div.input\").{}</script>"
+        output_args   = (javascript_functions[state],)
+        output        = output_string.format(*output_args)
+
+        display(HTML(output))
+
+
+    def button_action(value):
+
+        """
+        Calls the toggle_code function and updates the button description.
+        """
+
+        state = value.new
+
+        toggle_code(state)
+
+        value.owner.description = button_descriptions[state]
+
+
+    state = False
+    toggle_code(state)
+
+    button = widgets.ToggleButton(state, description = button_descriptions[state])
+    button.observe(button_action, "value")
+
+    display(button)
+    
 def printmd(S):
     display(Markdown(S))
     return
