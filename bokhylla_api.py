02af7ebe81dcf6418785c5992bb6cd7529c7b235
Lars G Johnsen <yoonsen@gmail.com>
2020-06-23 14:50:49 +0200

    Add files via upload

---
+++ bokhylla_api.py
@@ -0,0 +1,148 @@
+import dhlab.nbtext as nb
+import dhlab.graph_networkx_louvain as gnl
+import dhlab.nbtokenizer as tok
+from dhlab.module_update import css, update, code_toggle
+
+import requests
+import pandas as pd
+import matplotlib.pyplot as plt
+import networkx as nx
+import seaborn as sns
+%matplotlib inline
+
+def get_df(frases, title='aftenposten'):
+    import requests
+    querystring = " + ".join(['"'+frase+'"' for frase in frases])
+    query = {
+        'q':querystring,
+        'size':1,
+        'aggs':'year',
+        #'filter':'mediatype:{mt}'.format(mt=media),
+        'filter':'title:{title}'.format(title=title)
+    }
+    r = requests.get("https://api.nb.no/catalog/v1/items", params = query)
+    aggs = r.json()['_embedded']['aggregations'][0]['buckets']
+    return {x['key']:x['count'] for x in aggs}
+
+def get_json(frases, mediatype='aviser'):
+    import requests
+    querystring = " + ".join(['"'+frase+'"' for frase in frases])
+    query = {
+        'q':querystring,
+        'size':1,
+        'snippets':mediatype,
+        'aggs':'year',
+        
+#        'filter':'mediatype:{mt}'.format(mt=mediatype),
+        'searchType':'FULL_TEXT_SEARCH'
+        #'filter':'title:{title}'.format(title=title)
+    }
+    r = requests.get("https://api.nb.no/catalog/v1/items", params = query)
+    aggs = r.json()
+    return aggs
+
+def get_data(frase, media='avis', title='jazznytt'):
+    import requests
+    query = {
+        'q':'"'+frase+'""',
+        'size':1,
+        'aggs':'year',
+        'filter':'mediatype:{mt}'.format(mt=media),
+        'filter':'title:{title}'.format(title=title)
+    }
+    r = requests.get("https://api.nb.no/catalog/v1/items", params = query)
+    return r.json()
+
+def get_data_and(frases, title='aftenposten', media='avis'):
+    import requests
+    querystring = " + ".join(['"'+frase+'"' for frase in frases])
+    print(querystring)
+    query = {
+        'q':querystring,
+        'size':1,
+        'aggs':'year',
+        #'filter':'mediatype:{mt}'.format(mt=media),
+        'filter':'title:{title}'.format(title=title)
+    }
+    r = requests.get("https://api.nb.no/catalog/v1/items", params = query)
+    return r.json()
+
+def get_df_pd(frase, media='bøker'):
+    import pandas as pd
+    return pd.DataFrame.from_dict(get_df(frase, media=media ), orient='index').sort_index()
+
+def phrase_plots(phrase_sets, title='aftenposten', fra = 1960, til = 2020, step=5, rot=0, colours = ['r', 'b','g','y','m','c']):
+    df_all = []
+    for f in phrase_sets:
+        df_all.append(nb.frame(get_df(f, title= title), ', '.join(f)))
+    df = pd.concat(df_all, sort=False)
+    df.index = df.index.astype(int)
+    df = df.sort_index()
+    df['bins'] = pd.cut(df.index, range(fra, til, step), precision=0)
+    df.groupby('bins').sum().plot(kind='bar', color=colours, figsize=(15,5), rot=rot)
+    return
+
+def phrase_plots_anno(phrase_sets, title='aftenposten', fra = 1960, til = 2020, rot=0, colours = ['r', 'b','g']):
+    df_all = []
+    for f in phrase_sets:
+        df_all.append(nb.frame(get_df(f, title= title), ', '.join(f)))
+    df = pd.concat(df_all, sort=False)
+    df.index = df.index.astype(int)
+    df = df.sort_index()
+    #df['bins'] = pd.cut(df.index, range(fra, til, step), precision=0)
+    df.plot(kind='bar', figsize=(15,5), rot=rot, color=colours)
+    return
+
+def graph_from_df(df, threshold = 100):
+    edges =  []
+    normalizer = {(x, y): df.stack()[(x,x)]*df.stack()[(y,y)] for (x,y) in df.stack().index}
+    for (x, y) in df.stack().index:
+        if x != y:
+            if df.stack()[(x,y)] > threshold:
+                edges.append([x,y,df.stack()[(x,y)]/normalizer[(x,y)]])
+    G = nx.Graph()
+    G.add_weighted_edges_from(edges)
+    return G
+
+def get_konks(urn, phrase, window=1000, n = 1000):
+    import requests
+    querystring = '"'+ phrase +'"' 
+    query = {
+        'q':querystring,
+        'fragments': n,
+        'fragSize':window
+       
+    }
+    r = requests.get("https://api.nb.no/catalog/v1/items/{urn}/contentfragments".format(urn=urn), params = query)
+    res = r.json()
+    results = []
+    try:
+        for x in res['contentFragments']:
+            urn = x['pageid']
+            hit = x['text']
+            splits = hit.split('<em>')
+            s2 = splits[1].split('</em>')
+            before = splits[0]
+            word = s2[0]
+            after = s2[1]
+            results.append({'urn': urn, 'before': before, 'word':word, 'after':after})
+    except:
+        True
+    return results
+
+def get_phrase_info(urn, phrase, window=1000, n = 1000):
+    import requests
+    querystring = '"'+ phrase +'"' 
+    query = {
+        'q':querystring,
+       
+    }
+    r = requests.get("https://api.nb.no/catalog/v1/items/{urn}/contentfragments".format(urn=urn), params = query)
+    res = r.json()
+    return res
+
+def get_all_konks(term, urns):
+    konks = []
+    for u in urns:
+        konks += get_konks(u, term)
+    return konks
