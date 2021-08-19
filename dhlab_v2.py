b99da276eab96e868664bb81d2d7ffc1be83fd16
Lars G Johnsen <yoonsen@gmail.com>
2021-08-19 17:15:18 +0200

    ed

--- dhlab_v2.py
+++ dhlab_v2.py
@@ -8,28 +8,42 @@ BASE_URL1 = "https://api.nb.no/ngram/db1"
 pd.options.display.max_rows = 100
 
 
-
-def ngram_book(word = '.', title = None, period = None, publisher = None, lang=None, city = None, ddk = None, topic = None):
+def ngram_book(word = ['.'], title = None, period = None, publisher = None, lang=None, city = None, ddk = None, topic = None):
     """Get a time series for a word as string, title is name of book period is (year, year), lang is three letter iso code.
     Use % as wildcard where appropriate - no wildcards in word and lang"""
-    
     params = locals()
+    if isinstance(word, str):
+        # assume a comma separated string
+        word = [w.strip() for w in word.split(',')]
+    params['word'] = tuple(word)
     params = {x:params[x] for x in params if not params[x] is None}
     r = requests.post(BASE_URL1 + "/ngram_book", json = params)
-    df = pd.DataFrame.from_dict(r.json(), orient = 'index', columns =[word])
+    df = pd.DataFrame.from_dict(r.json(), orient = 'index')
+    df.index = df.index.map(lambda x: tuple(x.split()))
+    columns = df.index.levels[0]
+    df = pd.concat([df.loc[x] for x in columns], axis = 1)
+    df.columns = columns 
+    df.index = df.index.map(Timestamp)
+    return df
+
+def ngram_news(word = ['.'], title = None, period = None):
+    """ get a time series period is a tuple of (year, year), (yearmonthday, yearmonthday) 
+    word is string and title is the title of newspaper, use % as wildcard"""
+    params = locals()
+    if isinstance(word, str):
+        # assume a comma separated string
+        word = [w.strip() for w in word.split(',')]
+    params['word'] = tuple(word)
+    params = {x:params[x] for x in params if not params[x] is None}
+    r = requests.post(BASE_URL1 + "/ngram_newspapers", json = params)
+    df = pd.DataFrame.from_dict(r.json(), orient = 'index')
+    df.index = df.index.map(lambda x: tuple(x.split()))
+    columns = df.index.levels[0]
+    df = pd.concat([df.loc[x] for x in columns], axis = 1)
+    df.columns = columns
     df.index = df.index.map(pd.Timestamp)
     return df
 
-def ngram_news(word = '.', title = None, period = None):
-    """ get a time series period is a tuple of (year, year), (yearmonthday, yearmonthday) 
-    word is string and title is the title of newspaper, use % as wildcard"""
-    
-    params = locals()
-    params = {x:params[x] for x in params if not params[x] is None}
-    r = requests.post(BASE_URL1 + "/ngram_newspapers", json = params)
-    df = pd.DataFrame.from_dict(r.json(), orient = 'index', columns =[word])
-    df.index = df.index.map(pd.Timestamp)
-    return df
 
 def document_corpus(doctype = None, author = None,  from_year = None, to_year = None, title = None, ddk = None, subject = None, lang = None, limit = None):
     """ Fetch a corpus based on metadata - doctypes are digibok, digavis, digitidsskrift"""
