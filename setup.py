e955bf8bbb60c2af4509afed5245abffbeac201d
Ingerid Dale <ingerid.dale@nb.no>
2021-10-26 17:47:28 +0200

    feat: include dhlab modules and restructure package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dhlab",
-    version="1.0.0",
+    version="1.0.2",
     author="The National Library of Norway",
     author_email="dh-lab@nb.no",
     description="API for text and image analysis by the Digital Humanities lab (DH-lab)",
@@ -25,7 +25,21 @@ setuptools.setup(
     install_requires=[
         'msgpack',
         'wordcloud',
-        'python_louvain',
-        'beautifulsoup4'
+        'beautifulsoup4',
+        'ipython', 
+        'ipywidgets', 
+        'matplotlib', 
+        'networkx', 
+        'numpy',
+        'omelette',
+        'pandas', 
+        'pillow',
+        'pillow-simd',
+        'pylab-sdk',
+        'python-louvain',
+        'requests',
+        'scipy',
+        'seaborn',
+        'setuptools',
     ]
 )
