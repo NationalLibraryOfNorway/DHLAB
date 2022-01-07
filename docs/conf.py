a4f801dfd6c74158b908a5d342b00409a2ebb045
Ingerid Dale <ingerid.dale@nb.no>
2022-01-07 10:01:41 +0100

    fix: remove static path, remove fail on warning

--- docs/conf.py
+++ docs/conf.py
@@ -127,7 +127,7 @@ pygments_dark_style = "monokai"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'. See the documentation for
