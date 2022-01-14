c5deca501d4dd674db2f3120ee5c70982261cc4c
Lars G Johnsen <yoonsen@gmail.com>
2022-01-14 15:09:38 +0100

    Update ngram.py

--- dhlab/ngram/ngram.py
+++ dhlab/ngram/ngram.py
@@ -1,6 +1,35 @@
 from datetime import datetime
-from datetime import datetime
 
+from ..dhlab_api import ngram_book, ngram_news
+from ..nb_ngram import nb_ngram
+
+class Ngram():
+    def __init__(self, words = None, from_year = None, to_year = None, doctype = None, lang = 'nob'):
+        from datetime import datetime
+        
+        self.date = datetime.now()
+        if to_year is None:
+            to_year = self.date.year
+        if from_year is None:
+            from_year = 1950
+            
+        self.from_year = from_year
+        self.to_year = to_year
+        self.words = words
+        self.lang = lang
+        if not doctype is None:
+            if 'bok' in doctype:
+                doctype = 'bok'
+            elif 'avis' in doctype:
+                doctype = 'avis'
+            else:
+                doctype = 'bok'
+        else:
+            doctype = 'bok'
+        ngrm = nb_ngram(terms = ', '.join(words), corpus = doctype, years = (from_year, to_year))
+        ngrm.index = ngrm.index.astype(str)
+        self.ngram = ngrm
+        return None
 
 class Ngram_book(Ngram):
     """Extract ngrams using dynamics"""
