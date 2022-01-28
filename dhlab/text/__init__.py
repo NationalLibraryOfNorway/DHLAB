b54f032542db4ca84437c7ad9e9b6f37f5858fd9
yoonsen <yoonsen@gmail.com>
2022-01-29 00:15:45 +0100

    added export statement

--- dhlab/text/__init__.py
+++ dhlab/text/__init__.py
@@ -1,3 +1,4 @@
 from .conc_coll import Concordance, Collocations, Counts
 from .corpus import Corpus, Corpus_from_identifiers
 from .nbtokenizer import Tokens, tokenize
+from .chunking import Chunks
