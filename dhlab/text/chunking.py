6a65c218747334ed28610570f51ed5bac226adab
yoonsen <yoonsen@gmail.com>
2022-01-28 23:55:36 +0100

    autopep

--- dhlab/text/chunking.py
+++ dhlab/text/chunking.py
@@ -2,23 +2,24 @@ import pandas as pd
 
 from ..api.dhlab_api import get_chunks, get_chunks_para
 
+
 class Chunks():
     """Create chunks from a text parameters are urn and chunks
     where chunks is either a number or the string starts with para or avsn"""
-    
-    def __init__(self, urn = None, chunks = 1000):
+
+    def __init__(self, urn=None, chunks=1000):
         # switch function based on value of chunks - a number or a string
         # indicating whether each paragraph is a chunk, else chop up the text
         # in pieces according to chunks
-        
+
         if isinstance(chunks, str):
             if chunks.startswith("para") or chunks.startswith("avsn"):
-                self.chunks = get_chunks_para(urn = urn)
+                self.chunks = get_chunks_para(urn=urn)
             else:
                 self.chunks = {}
         else:
-            get_chunks(urn = urn, chunk_size = chunks)
-    
+            get_chunks(urn=urn, chunk_size=chunks)
+
     def to_pandas(self):
         """Vectorize into a pandas dataframe with words a index"""
         return pd.DataFrame(self.chunks).transpose()
--- dhlab/text/corpus.py
+++ dhlab/text/corpus.py
@@ -6,20 +6,20 @@ from ..api.dhlab_api import document_corpus, get_metadata
 
 class Corpus():
     def __init__(
-        self,
-        doctype = None,
-        author = None,
-        freetext = None,
-        from_year = None,
-        to_year = None,
-        from_timestamp = None,
-        to_timestamp = None,
-        title = None,
-        ddk = None,
-        subject = None,
-        lang = None,
-        limit = None):
-        
+            self,
+            doctype=None,
+            author=None,
+            freetext=None,
+            from_year=None,
+            to_year=None,
+            from_timestamp=None,
+            to_timestamp=None,
+            title=None,
+            ddk=None,
+            subject=None,
+            lang=None,
+            limit=None):
+
         self.corpus = document_corpus(
             doctype,
             author,
@@ -34,19 +34,20 @@ class Corpus():
             lang,
             limit
         )
-        
+
         self.size = len(self.corpus)
-        
+
         return
-    
+
+
 class Corpus_from_identifiers(Corpus):
-    def __init__(self, identifiers = None):
+    def __init__(self, identifiers=None):
         self.corpus = get_metadata(urnlist(identifiers))
-        
+
 
 def urnlist(corpus):
     """Try to pull out a list of URNs from corpus"""
-    
+
     if isinstance(corpus, Corpus):
         urnlist = list(corpus.corpus.urn)
     elif isinstance(corpus, DataFrame):
