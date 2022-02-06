391a96132884d1ecfa6c6d5f39a96debeaa5b4ba
Lars G Johnsen <yoonsen@gmail.com>
2022-02-06 23:43:09 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dhlab",
-    version="2.0.15",
+    version="2.0.16",
     author="The National Library of Norway",
     author_email="dh-lab@nb.no",
     description="Library for text and image analysis by the Digital Humanities lab (DH-lab)",
