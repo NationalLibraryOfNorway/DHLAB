9087e6e17563c63e70e9827ce03ff343bd1972cf
Yoonsen <Yoonsen@gmail.com>
2022-02-21 14:00:20 +0100

    bugfixes

--- dhlab/text/dispersion.py
+++ dhlab/text/dispersion.py
@@ -2,15 +2,16 @@ import pandas as pd
 from ..api.dhlab_api import get_dispersion
 
 class Dispersion():
-    def __init__(self, urn=None, wordbags=None, window=1000, pr=100):
-        if isinstance(wordbags, list):
-            self.dispersion = {w:get_dispersion(urn, words=[w], window=window, pr=pr) for w in wordbags}
-        elif isinstance(wordbags, dict):
-            self.dispersion = {w:get_dispersion(urn, words=wordbags[w], window=window, pr=pr) for w in wordbags}
-        elif isinstance(wordbags,str):
-            self.dispersion = {w:get_disperion(urn, words=[w], window=window, pr=pr)}
-        else
-            self.dispersion = {}
-            
-    def plot(**kwargs):
+    def __init__(self, urn=None, wordbag=None, window=1000, pr=100):
+        if isinstance(wordbag, list):
+            dispersion = {w:get_dispersion(urn, words=[w], window=window, pr=pr) for w in wordbag}
+        elif isinstance(wordbag, dict):
+            dispersion = {w:get_dispersion(urn, words=wordbag[w], window=window, pr=pr) for w in wordbag}
+        elif isinstance(wordbag,str):
+            dispersion = {wordbag: get_dispersion(urn, words=[wordbag], window=window, pr=pr) }
+        else:
+            dispersion = {}
+        self.dispersion = pd.DataFrame(dispersion)
+        
+    def plot(self, **kwargs):
         self.dispersion.plot()
