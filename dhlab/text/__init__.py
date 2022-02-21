679c57ce2bc085834d99d1c26a474355c2539eae
Yoonsen <Yoonsen@gmail.com>
2022-02-21 13:00:06 +0100

    edit init setup

--- dhlab/text/__init__.py
+++ dhlab/text/__init__.py
@@ -1,4 +1,5 @@
 from .conc_coll import Concordance, Collocations, Counts
 from .corpus import Corpus, Corpus_from_identifiers, Corpus_from_identifiers as CorpusFromIdentifiers
+from .dispersion import Dispersion
 from .nbtokenizer import Tokens, tokenize
 from .chunking import Chunks
