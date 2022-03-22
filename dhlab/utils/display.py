2350bbeca449af7047614ece067d5fc792f4cc60
Ingerid Dale <ingerid.dale@nb.no>
2022-03-22 17:39:44 +0100

    style(pep8): remove trailing whitespace
    Signed-off-by: Ingerid Dale <ingerid.dale@nb.no>

--- dhlab/utils/display.py
+++ dhlab/utils/display.py
@@ -4,12 +4,12 @@ import requests
 from IPython.display import HTML
 
 
-def code_toggle(button_text = "Klikk for å vise/skjule kodeceller"):
+def code_toggle(button_text="Klikk for å vise/skjule kodeceller"):
     from IPython.display import HTML, display
 
     display(
         HTML(
-        '''<div>
+            '''<div>
                 <style>
                  .mybutton {
                     background-color: lightgrey;
@@ -37,34 +37,34 @@ def code_toggle(button_text = "Klikk for å vise/skjule kodeceller"):
                 $( document ).ready(code_toggle);
             </script>
             <form  action="javascript:code_toggle()">
-                <input class='mybutton' type="submit" value=''' + '"'  + button_text + '"' + '''>
+                <input class='mybutton' type="submit" value=''' + '"' + button_text + '"' + '''>
             </form>
         </div>'''
         ))
 
 
-def css(url = "https://raw.githubusercontent.com/Yoonsen/Modules/master/css_style_sheets/nb_notebook.css"):
+def css(url="https://raw.githubusercontent.com/Yoonsen/Modules/master/css_style_sheets/nb_notebook.css"):
     """Associate a css stylesheet with the notebook, 
     just specify a file or web reference, default is a custom css"""
-    
+
     uri = urlparse(url)
     css_file = ""
-    
+
     if uri.scheme.startswith('http'):
         query = requests.get(url)
         if query.status_code == 200:
-            css_file  = query.text
-    
-    elif uri.scheme == "file": 
+            css_file = query.text
+
+    elif uri.scheme == "file":
         # assume on form "file:/// on windows there is drive letter on unix not"
         file_path = url[7:]
-        if file_path[2] == ':': # then windows drive reference
+        if file_path[2] == ':':  # then windows drive reference
             file_path = file_path[1:]
         with open(file_path, encoding='utf-8') as file:
             css_file = file.read()
-    else: 
+    else:
         # assume string is a file locator
         with open(url, encoding='utf-8') as file:
             css_file = file.read()
-    
-    return HTML("<style>{css_code}</style>".format(css_code = css_file))
+
+    return HTML("<style>{css_code}</style>".format(css_code=css_file))
