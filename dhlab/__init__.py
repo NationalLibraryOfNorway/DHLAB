53343280f6955d06e740133c093de44aa9d3898b
ingerid <37677043+Ingerid@users.noreply.github.com>
2022-03-22 09:28:56 +0100

    fix: import all legacy modules in __init__.py

--- dhlab/__init__.py
+++ dhlab/__init__.py
@@ -1,3 +1,14 @@
 # legacy code
+from dhlab.text import nbtokenizer
+from dhlab.legacy import (
+    nbtext,
+    graph_networkx_louvain,
+    token_map,
+    nbpictures,
+    nb_external_files,
+    module_update
+)
 
 # code from further down in the code tree
+from dhlab.text.corpus import Corpus
+from dhlab.text.conc_coll import Collocations, Concordance
