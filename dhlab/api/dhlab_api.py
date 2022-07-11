56f41b553d87dd6f96d5a3b63fc203d36d155c6b
Yoonsen <Yoonsen@gmail.com>
2022-07-11 14:36:21 +0200

    :fix: get frequencies

--- dhlab/api/dhlab_api.py
+++ dhlab/api/dhlab_api.py
@@ -208,15 +208,19 @@ def get_document_frequencies(urns=None, cutoff=0, words=None):
     params = locals()
     r = requests.post(f"{BASE_URL}/frequencies", json=params)
     result = r.json()
-    structure = {u[0]:dict([tuple(u[1:3])]) for u in result}
+    structure = {u[0][0]:dict([tuple(u[0][1:3])]) for u in result}
     df = pd.DataFrame(structure)
     return df.sort_values(by=df.columns[0], ascending=False)
 
 
 
 def get_word_frequencies(urns=None, cutoff=0, words=None):
+    """Get frequency numbers from documents as a list of URNs 
+    :urns: list of urns
+    :cutoff: minimum frequency of a word to be counted
+    :word: a list of words to be counted - if left None, whole document is returned"""
     params = locals()
-    r = requests.post(BASE_URL + "/frequencies", json=params)
+    r = requests.post(f"{BASE_URL}/frequencies", json=params)
     result = r.json()
     structure = {u[0][0]: dict([(x[1], x[2] / x[3]) for x in u])
                  for u in result if u != []}
