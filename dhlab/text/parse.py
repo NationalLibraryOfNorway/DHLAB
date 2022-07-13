46ef1e1d29c8b4f188514f574ae4cb240c5a7063
Yoonsen <Yoonsen@gmail.com>
2022-07-13 15:06:04 +0200

    fix: name error

--- dhlab/text/parse.py
+++ dhlab/text/parse.py
@@ -1,7 +1,7 @@
 import spacy
 import pandas as pd
 
-from dhlab.api.dhlab_api import ner_urn, show_spacy_models
+from dhlab.api.dhlab_api import ner_from_urn, show_spacy_models
 
 class Models:
     def __init__(self):
@@ -10,5 +10,5 @@ class Models:
 class NER:
     def __init__(self, urn=None, model=None):
         self.model = model
-        self.ner = ner_urn(urn=urn, model=self.spacy_model)
+        self.ner = ner_from_urn(urn=urn, model=self.spacy_model)
 
