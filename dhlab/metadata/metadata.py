c91b1bf931413c460d45c8284747953ef2f35e96
Ingerid Dale <ingerid.dale@nb.no>
2022-09-12 15:31:38 +0200

    check event trigger for bump fails to bump
    Signed-off-by: Ingerid Dale <ingerid.dale@nb.no>

--- dhlab/metadata/metadata.py
+++ dhlab/metadata/metadata.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import requests
+from typing import List
 
 from dhlab.constants import BASE_URL
 
 
-def get_metadata(urns=None):
-    """ Fetch metadata from a list of urns """
+def get_metadata(urns: List = None):
+    """Fetch metadata from a list of urns.
+
+    :param list urns: uniform resource names, example: ``["URN:NBN:no-nb_digibok_2011051112001", ...]``
+    """
     params = locals()
     r = requests.post(f"{BASE_URL}/get_metadata", json=params)
     return pd.DataFrame(r.json())
