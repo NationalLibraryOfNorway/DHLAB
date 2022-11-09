491d361b9fc326a866e2526dcc3e6936355da2ef
LarsM <larsmtungland@gmail.com>
2022-11-09 14:48:26 +0100

    refactor(corpus): change add()
    * new add() as suggested by Ingerid
    * refactor extend from identifiers

--- dhlab/text/corpus.py
+++ dhlab/text/corpus.py
@@ -2,7 +2,7 @@ from pandas import DataFrame
 import pandas as pd
 from dhlab.text.dhlab_object import DhlabObj
 from dhlab.api.dhlab_api import document_corpus, get_metadata, evaluate_documents
-from typing import Union
+
 
 class Corpus(DhlabObj):
     """Class representing as DHLAB Corpus"""
@@ -23,7 +23,7 @@ class Corpus(DhlabObj):
             limit=10,
             ):
 
-        if (doctype 
+        if (doctype
             or author
             or freetext
             or fulltext
@@ -34,7 +34,7 @@ class Corpus(DhlabObj):
             or title
             or ddk
             or lang):
-            
+
             self.corpus = document_corpus(
                 doctype,
                 author,
@@ -50,10 +50,10 @@ class Corpus(DhlabObj):
                 lang,
                 limit
             )
-            
+
         else:
             self.corpus = pd.DataFrame(columns=["urn"])
-        
+
         super().__init__(self.corpus)
         self.urn = self.corpus.urn
         self.size = len(self.corpus)
@@ -61,43 +61,34 @@ class Corpus(DhlabObj):
     @classmethod
     def from_df(cls, df):
         """Typecast Pandas DataFrame to Corpus class
-        
-        DataFrame most contain URN column"""        
+
+        DataFrame most contain URN column"""
         corpus = Corpus()
         corpus.corpus = cls._urn_id_in_dataframe_cols(df)
         corpus.urn = corpus.corpus.urn
         corpus.frame = corpus.corpus
         corpus.size = len(corpus.corpus)
         return corpus
-    
+
     @classmethod
     def from_csv(cls, path):
         """Import corpus from csv"""
         df = pd.read_csv(path)
         return cls.from_df(df)
-        
+
     @staticmethod
     def _urn_id_in_dataframe_cols(dataframe):
         """Checks if dataframe contains URN column"""
-        
+
         if "urn" in dataframe.columns:
             if dataframe.urn.str.contains("^URN:NBN:no-nb_.+").all():
-                return dataframe                    
-        raise ValueError("No'urn'-column in dataframe.")       
-    
-    def to_csv(self, path):
-        self.corpus.to_csv(path, index=None)        
+                return dataframe
+        raise ValueError("No'urn'-column in dataframe.")
 
-    def add(self, corpus = None):
-        """Add a corpus to existing corpus"""
-        self.corpus = pd.concat([self.corpus, corpus.corpus]).drop_duplicates().reset_index(drop=True)
-        self.size = len(self.corpus)
-    
     def extend_from_identifiers(self, identifiers=None):
-        corpus = get_metadata(urnlist(identifiers))
-        self.corpus = pd.concat([self.corpus, corpus]).drop_duplicates().reset_index(drop=True)
-        self.size = len(self.corpus)
-        
+        new_corpus = get_metadata(urnlist(identifiers))
+        self.add(new_corpus)
+
     def evaluate_words(self, wordbags = None):
         df = evaluate_documents(wordbags = wordbags, urns = list(self.corpus.urn))
         df.index = df.index.astype(int)
@@ -105,21 +96,35 @@ class Corpus(DhlabObj):
         df = pd.concat([df, self.corpus[['dhlabid','urn']].set_index('dhlabid')], axis = 1)
         df = df.set_index('urn')
         return df[cols].fillna(0)
-    
+
+    def add(self, new_corpus):
+        """Utility for appending Corpus or DataFrame to self"""
+        if self._is_Corpus(new_corpus):
+            new_corpus = new_corpus.frame
+        self.frame = pd.concat([self.frame, new_corpus]).drop_duplicates().reset_index(drop=True)
+        self.corpus = self.frame
+        self.size = len(self.frame)
+        self.urn = self.frame.urn
+
     def sample(self, n=5):
         "Create random subkorpus with `n` entries"
-        # if n >= self.size:            
         n = min(n, self.size)
-            #raise ValueError(f"Sample must be smaller than main corpus ({self.size})")        
         sample = self.corpus.sample(n).copy()
         return Corpus.from_df(sample)
-    
+
+    @staticmethod
+    def _is_Corpus(corpus) -> bool:
+        """Check if `input` is Corpus or DataFrame"""
+        if type(corpus) not in [DataFrame, Corpus]:
+            raise TypeError("Input is not Corpus or DataFrame")
+        return isinstance(corpus, Corpus)
+
 class EmptyCorpus(Corpus):
     """DEPRECATED: call Corpus without parameters to represent an empty corpus"""
     def __init__(self):
         self.corpus = pd.DataFrame()
         self.size = 0
-        
+
 class Corpus_from_identifiers(Corpus):
     def __init__(self, identifiers=None):
         self.corpus = get_metadata(urnlist(identifiers))
@@ -127,13 +132,12 @@ class Corpus_from_identifiers(Corpus):
 
 def urnlist(corpus):
     """Try to pull out a list of URNs from corpus"""
-
     if isinstance(corpus, Corpus):
-        urnlist = list(corpus.corpus.urn)
+        _urnlist = list(corpus.corpus.urn)
     elif isinstance(corpus, DataFrame):
-        urnlist = list(corpus.urn)
+        _urnlist = list(corpus.urn)
     elif isinstance(corpus, list):
-        urnlist = corpus
+        _urnlist = corpus
     else:
-        urnlist = []
-    return urnlist
+        _urnlist = []
+    return _urnlist
