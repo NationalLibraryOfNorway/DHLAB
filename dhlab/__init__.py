e0bdce0d05e48f896f6047c73b0f147c311f98f3
yoonsen <yoonsen@gmail.com>
2023-02-16 17:10:29 +0100

    fix:floated display and files up to dhlab

--- dhlab/__init__.py
+++ dhlab/__init__.py
@@ -18,6 +18,8 @@ from dhlab.text.conc_coll import Collocations, Concordance, Counts
 from dhlab.text.geo_data import GeoData, GeoNames
 from dhlab.text.parse import NER, POS, Models
 from dhlab.text.wildcard import WildcardWordSearch
+from dhlab.utils.display import css
+from dhlab.utils.files import download_from_github,get_file_from_github
 
 # ngram 
 from dhlab.ngram.ngram import Ngram, NgramBook, NgramNews
