a2fad297b08011beb5df3dcd64d2150a39f1a364
Lars Magne Tungland <larsmtungland@gmail.com>
2023-07-28 13:55:25 +0200

    rm removed modules from init file

--- dhlab/__init__.py
+++ dhlab/__init__.py
@@ -1,31 +1,15 @@
-# legacy code
-from dhlab.text import nbtokenizer
-from dhlab.legacy import (
-    nbtext,
-    graph_networkx_louvain,
-    token_map,
-    nbpictures,
-    nb_external_files,
-    module_update
-)
-
 # code from further down in the code tree
 
 # text 
 from dhlab.text.corpus import Corpus
 from dhlab.text.chunking import Chunks
 from dhlab.text.conc_coll import Collocations, Concordance, Counts
-from dhlab.text.geo_data import GeoData, GeoNames
-from dhlab.text.parse import NER, POS, Models
 from dhlab.text.wildcard import WildcardWordSearch
 from dhlab.utils.display import css
 from dhlab.utils.files import download_from_github,get_file_from_github
 
 # ngram 
 from dhlab.ngram.ngram import Ngram, NgramBook, NgramNews
-
-# wordbank 
-from dhlab.wordbank.wordbank import WordParadigm, WordLemma, WordForm
 
 # api
 from dhlab.api.dhlab_api import totals
