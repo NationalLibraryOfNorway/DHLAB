4bc058c1c53a55cfb283018d1d2647b8024808d3
Lars Magne Tungland <larsmtungland@gmail.com>
2023-07-31 12:18:22 +0200

    update utils

--- dhlab/text/utils.py
+++ dhlab/text/utils.py
@@ -4,7 +4,7 @@ import dhlab as dh
 def urnlist(corpus):
     """Try to pull out a list of URNs from corpus"""
     if isinstance(corpus, dh.Corpus):
-        _urnlist = list(corpus.corpus.urn)
+        _urnlist = list(corpus.urn)
     elif isinstance(corpus, DataFrame):
         _urnlist = list(corpus.urn)
     elif isinstance(corpus, list):
@@ -14,3 +14,8 @@ def urnlist(corpus):
     else:
         _urnlist = []
     return _urnlist
+
+# convert cell to a link
+def make_link(row):
+    r = "<a target='_blank' href = 'https://urn.nb.no/{x}'>{x}</a>".format(x=str(row))
+    return r
