f4ff0751f3c397b078624d80154c3e20fbfd9949
Lars Magne Tungland <larsmtungland@gmail.com>
2023-07-31 10:32:36 +0200

    edit text/init.py

--- dhlab/text/__init__.py
+++ dhlab/text/__init__.py
@@ -1,9 +1,1 @@
-"""Text analysis functionality, including building text corpora."""
-from .chunking import Chunks
-from .conc_coll import Concordance, Collocations, Counts
-from .corpus import Corpus
-from .dispersion import Dispersion
-from .geo_data import GeoData
-from .nbtokenizer import Tokens, tokenize
-from .parse import NER, POS, Models
-from .wildcard import WildcardWordSearch
+
