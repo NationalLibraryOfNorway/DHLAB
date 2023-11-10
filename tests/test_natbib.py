32cca8894620d350dd3072798d7aee220f1c99f2
Lars Magne Tungland <larsmtungland@gmail.com>
2023-11-10 15:41:15 +0100

    refactor: formatting

--- tests/test_natbib.py
+++ tests/test_natbib.py
@@ -1,18 +1,20 @@
 from dhlab.metadata.natbib import metadata_query, metadata_from_urn
 import pandas as pd
 
+
 def test_metadata_query():
     conditions = [
         ["245", "a", "kongen"],
-       # ["008", "", "nno"]
-        ]
+        # ["008", "", "nno"]
+    ]
     res = metadata_query(conditions)
     assert len(res) > 0
     assert isinstance(res, list)
-    
+
+
 def test_metadata_from_urn():
     pass
 
+
 def test_metadata():
     pass
-    
