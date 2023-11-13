9c88a2cb52aaf5fcc246771120f250edeb8a1d17
Ingerid Dale <ingerid.dale@nb.no>
2023-11-13 16:01:38 +0100

    docs: remove invalid escape sequence '\*' + black reformatting

--- dhlab/ngram/ngram.py
+++ dhlab/ngram/ngram.py
@@ -3,18 +3,21 @@ from datetime import datetime
 from dhlab.api.dhlab_api import ngram_book, ngram_news
 from dhlab.ngram.nb_ngram import nb_ngram
 from dhlab.text.dhlab_object import DhlabObj
+
+
 class Ngram(DhlabObj):
     """Top level class for ngrams"""
 
-    def __init__(self,
-                 words=None,
-                 from_year=None,
-                 to_year=None,
-                 doctype='bok',
-                 mode='relative',
-                 lang="nob",
-                 **kwargs
-                 ):
+    def __init__(
+        self,
+        words=None,
+        from_year=None,
+        to_year=None,
+        doctype="bok",
+        mode="relative",
+        lang="nob",
+        **kwargs
+    ):
         """Ngram builder class.
 
         Build Ngrams from the National Librarys collections.
@@ -34,7 +37,7 @@ class Ngram(DhlabObj):
         :type mode: str, optional
         :param lang: `nob`, `nno`. Only use with docytype='bok', defaults to 'nob'
         :type lang: str, optional
-        :param \**kwargs: Keyword arguments for  Ngram._ipython_display_() Ngram.plot()
+        :param kwargs: Keyword arguments for  Ngram._ipython_display_() Ngram.plot()
         """
 
         self.date = datetime.now()
@@ -48,25 +51,27 @@ class Ngram(DhlabObj):
         self.words = words
         self.lang = lang
         if doctype is not None:
-            if 'bok' in doctype:
-                doctype = 'bok'
-            elif 'avis' in doctype:
-                doctype = 'avis'
+            if "bok" in doctype:
+                doctype = "bok"
+            elif "avis" in doctype:
+                doctype = "avis"
             else:
-                doctype = 'bok'
+                doctype = "bok"
         else:
-            doctype = 'bok'
+            doctype = "bok"
 
         # Set default lang for 'bok'-corpus
         if doctype == "avis":
             lang = None
 
-
-        ngrm = nb_ngram(terms=', '.join(words),
-                        corpus=doctype,
-                        years=(from_year, to_year),
-                        smooth = 1, lang = lang,
-                        mode=mode)
+        ngrm = nb_ngram(
+            terms=", ".join(words),
+            corpus=doctype,
+            years=(from_year, to_year),
+            smooth=1,
+            lang=lang,
+            mode=mode,
+        )
         ngrm.index = ngrm.index.astype(str)
         self.ngram = ngrm
 
@@ -74,40 +79,46 @@ class Ngram(DhlabObj):
 
         super().__init__(self.ngram)
 
-    def plot(self, smooth = 4, **kwargs):
+    def plot(self, smooth=4, **kwargs):
         """:param smooth: smoothing the curve"""
-        grf = self.ngram.rolling(window=smooth, win_type='triang').mean()
+        grf = self.ngram.rolling(window=smooth, win_type="triang").mean()
         grf.plot(**kwargs)
 
     def compare(self, another_ngram):
         """Divide one ngram by another - measures difference"""
-        start_year = max(datetime(self.from_year, 1, 1),
-                         datetime(another_ngram.from_year, 1, 1)).year
-        end_year = min(datetime(self.to_year, 1, 1), datetime(another_ngram.to_year, 1, 1)).year
-        transposed_ngram = self.ngram.loc[str(start_year):str(end_year)].transpose()
-        sum_other_ngram = another_ngram.ngram[str(start_year):str(end_year)].transpose().sum()
+        start_year = max(
+            datetime(self.from_year, 1, 1), datetime(another_ngram.from_year, 1, 1)
+        ).year
+        end_year = min(
+            datetime(self.to_year, 1, 1), datetime(another_ngram.to_year, 1, 1)
+        ).year
+        transposed_ngram = self.ngram.loc[str(start_year) : str(end_year)].transpose()
+        sum_other_ngram = (
+            another_ngram.ngram[str(start_year) : str(end_year)].transpose().sum()
+        )
         compare = (transposed_ngram / sum_other_ngram).transpose()
         return compare
 
     def _ipython_display_(self):
         self.plot(**self.kwargs)
 
+
 class NgramBook(Ngram):
     """Extract ngrams using metadata with functions to be inherited."""
 
     def __init__(
-            self,
-            words=None,
-            title=None,
-            publisher=None,
-            city=None,
-            lang='nob',
-            from_year=None,
-            to_year=None,
-            ddk=None,
-            subject=None,
-            **kwargs
-        ):
+        self,
+        words=None,
+        title=None,
+        publisher=None,
+        city=None,
+        lang="nob",
+        from_year=None,
+        to_year=None,
+        ddk=None,
+        subject=None,
+        **kwargs
+    ):
         """Create Dhlab Ngram from metadata
 
         :param words: words to examine, defaults to None
@@ -132,8 +143,14 @@ class NgramBook(Ngram):
         :rtype: _type_
         """
 
-        super().__init__(words, from_year = from_year,
-                         to_year = to_year, lang = lang, doctype = 'bok', **kwargs)
+        super().__init__(
+            words,
+            from_year=from_year,
+            to_year=to_year,
+            lang=lang,
+            doctype="bok",
+            **kwargs
+        )
         self.date = datetime.now()
         if to_year is None:
             to_year = self.date.year
@@ -148,8 +165,16 @@ class NgramBook(Ngram):
         self.lang = lang
         self.ddk = ddk
         self.subject = subject
-        self.ngram = ngram_book(word=words, title=title, publisher=publisher, lang=lang, city=city,
-                                period=(from_year, to_year), ddk=ddk, topic=subject)
+        self.ngram = ngram_book(
+            word=words,
+            title=title,
+            publisher=publisher,
+            lang=lang,
+            city=city,
+            period=(from_year, to_year),
+            ddk=ddk,
+            topic=subject,
+        )
         # update frame attribute
         self.frame = self.ngram
         # self.cohort =  (self.ngram.transpose()/self.ngram.transpose().sum()).transpose()
@@ -157,15 +182,11 @@ class NgramBook(Ngram):
 
 class NgramNews(Ngram):
     def __init__(
-            self,
-            words=None,
-            title=None,
-            city=None,
-            from_year=None,
-            to_year=None,
-            **kwargs
+        self, words=None, title=None, city=None, from_year=None, to_year=None, **kwargs
     ):
-        super().__init__(words, from_year = from_year, to_year = to_year, doctype = 'avis', **kwargs)
+        super().__init__(
+            words, from_year=from_year, to_year=to_year, doctype="avis", **kwargs
+        )
         self.date = datetime.now()
         self.from_year = 1950 if from_year is None else from_year
         self.to_year = self.date.year if to_year is None else to_year
