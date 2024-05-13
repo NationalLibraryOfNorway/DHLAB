5f770f52ab2d9510ed3aea591d04533160bd67e1
Ingerid Dale <ingerid.dale@nb.no>
2024-05-13 17:26:37 +0200

    style: add new logo and use DM Mono+Sans fonts
    add github logo + repo link in footer

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -133,6 +133,9 @@ html_theme_options = {
         "color-brand-primary": "#0044ba",  # "#cf2e2e", #015edf;
         "color-brand-content": "#0044ba",  # 00215c",  # "#0693e3",
         "color-admonition-background": "#70a6ff",  # "#edeae5",
+        "font-stack": "'DM Sans', sans-serif",
+        "font-stack--monospace": "Courier, monospace",
+        "font-stack--headings": "'DM Mono', monospace",
     },
     # "dark_logo": "_images/NB-symbol-logo_blaa_mork_100width.png",
     "dark_css_variables": {
@@ -142,6 +145,18 @@ html_theme_options = {
     },
     # "top_of_page_button": "edit",
     # For components/edit-this-page.html
+    "footer_icons": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/NationalLibraryOfNorway/DHLAB",
+            "html": """
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+                </svg>
+            """,
+            "class": "",
+        },
+    ],
 }
 
 
@@ -165,18 +180,12 @@ html_short_title = "dhlab"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-# html_logo = "_images/DHLAB-logo-no-eng-blaa-0044ba.png"  # blue
-# html_logo = "_images/NB-symbol-logo_blaa_mork.png"
-# html_logo = "_images/NB-symbol-logo_blaa_mork_100width.png"
-# html_logo = "_images/DHlab_logo_web_en_black.png"
-html_logo = "_images/dhlab_logo.png"  # red
+html_logo = "_images/favicon.svg"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-# html_favicon = "_images/nb_symbol_farge_z5B_icon.ico"  # red
-# html_favicon = "https://www.nb.no/content/uploads/2024/01/cropped-Nasjonalbiblioteket-—-Logo-—-Bla-—-Hvit-bakgrunn-32x32.png"  # bright blue
-html_favicon = "_images/NB-symbol-logo_blaa_mork.png"  # dark blue #0044ba
+html_favicon = "_images/favicon.svg"  # dark blue #0044ba
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
