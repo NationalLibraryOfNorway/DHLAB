fe5d1a0e4ad06b2083bf32767d2fa7764f378e6e
Magnus Breder Birkenes <magnus.birkenes@nb.no>
2024-08-01 15:36:16 +0200

    feat: use python-dotenv for config

--- dhlab/constants.py
+++ dhlab/constants.py
@@ -1,5 +1,10 @@
-"""Constant variables used across modules."""
+import os
+from dotenv import load_dotenv
 
-BASE_URL = "https://api.nb.no/dhlab"  #: REST-API URL adress to fulltext query functions
-NGRAM_API = "https://api.nb.no/dhlab/nb_ngram/ngram/query"  #: URL adress for API calls to ngram-databases
-GALAXY_API = "https://api.nb.no/dhlab/nb_ngram_galaxies/galaxies/query"  #: URL adress for word galaxy API queries
+load_dotenv("dhlab.env")  # take environment variables
+
+"""Constant variables used across modules, change in dhlab.env or through environment variable"""
+
+BASE_URL = os.getenv("NB_DHLAB_BASE_URL")  #: REST-API URL adress to fulltext query functions
+NGRAM_API =  os.getenv("NB_DHLAB_NGRAM_API") #: URL adress for API calls to ngram-databases
+GALAXY_API = os.getenv("NB_DHLAB_GALAXY_API")  #: URL adress for word galaxy API queries
