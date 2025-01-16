961f586189b8b34b1746868cf0ae7aeef0ede70e
iciclejj <55782191+iciclejj@users.noreply.github.com>
2025-01-16 16:21:18 +0100

    Remove unused file

--- tests/test_natbib2.py
+++
@@ -1,8 +0,0 @@
-from dhlab.metadata.natbib2 import QueryBuilder
-
-# TODO?
-# def test_querybuilder():
-#     b = QueryBuilder()
-#     res = b.add_field("100",  "a", "Ibsen").add_field("700", "a", "Ibsen").add_field("008", value="eng").set_return_fields("fields.100").set_limit(100).post()
-#     assert isinstance(res, list)
-#     assert len(res) > 0
