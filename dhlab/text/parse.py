aa16244561c807cf5efd663b242fa57dd4d77d47
iciclejj <55782191+iciclejj@users.noreply.github.com>
2025-05-15 13:21:00 +0200

    XXX SQUASH: Type-hint dhlab/text/parse.py

--- dhlab/text/parse.py
+++ dhlab/text/parse.py
@@ -1,5 +1,3 @@
-import pandas as pd
-
 from dhlab.api.dhlab_api import ner_from_urn, pos_from_urn, show_spacy_models
 
 
@@ -13,7 +11,13 @@ class Models:
 class NER:
     """Provide NER"""
 
-    def __init__(self, urn=None, model=None, start_page=0, to_page=0):
+    def __init__(
+        self,
+        urn: str | None = None,
+        model: str | None = None,
+        start_page: int = 0,
+        to_page: int = 0
+    ):
         self.model = model
         self.ner = ner_from_urn(
             urn=urn, model=self.model, start_page=start_page, to_page=to_page
@@ -23,7 +27,13 @@ class NER:
 class POS:
     """Provide POS and a parse"""
 
-    def __init__(self, urn=None, model=None, start_page=0, to_page=0):
+    def __init__(
+        self,
+        urn: str | None = None,
+        model: str | None = None,
+        start_page: int = 0,
+        to_page: int = 0
+    ):
         self.model = model
         self.pos = pos_from_urn(
             urn=urn, model=self.model, start_page=start_page, to_page=to_page
