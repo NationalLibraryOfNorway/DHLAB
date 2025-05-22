8a594b8d5776a8e338728803e91880ef5707f75a
iciclejj <55782191+iciclejj@users.noreply.github.com>
2025-05-22 11:00:11 +0200

    Enforce arguments required by the api

--- dhlab/api/dhlab_api.py
+++ dhlab/api/dhlab_api.py
@@ -558,7 +558,7 @@ def _create_sparse_matrix(structure):
     return df_sparse
 
 def get_document_frequencies(
-    urns: List[str] | None = None, cutoff: int = 0, words: List[str] | None = None, sparse: bool = False
+    urns: List[str], cutoff: int = 0, words: List[str] | None = None, sparse: bool = False
 ) -> DataFrame:
     """Fetch frequency counts of ``words`` in documents (``urns``).
 
@@ -600,7 +600,7 @@ def get_document_frequencies(
 
 
 def get_word_frequencies(
-    urns: List[str] | None = None, cutoff: int = 0, words: List[str] | None = None
+    urns: List[str], cutoff: int = 0, words: List[str] | None = None
 ) -> DataFrame:
     """Fetch frequency numbers for ``words`` in documents (``urns``).
 
@@ -615,7 +615,7 @@ def get_word_frequencies(
     return get_document_frequencies(urns, cutoff, words)
 
 
-def get_urn_frequencies(urns: List[str] | None = None, dhlabid: List[int] | None = None) -> DataFrame:
+def get_urn_frequencies(urns: List[str], dhlabid: List[int] | None = None) -> DataFrame:
     """Fetch frequency counts of documents as URNs or DH-lab ids.
 
     Call the API :py:obj:`~dhlab.constants.BASE_URL` endpoint
@@ -663,6 +663,8 @@ def document_corpus(
     Call the API :py:obj:`~dhlab.constants.BASE_URL` endpoint
     `/build_corpus <https://api.nb.no/dhlab/#/default/post_build_corpus>`_.
 
+    At least one filter argument must be set.
+
     :param str doctype: ``"digibok"``, ``"digavis"``, ``"digitidsskrift"`` or ``"digistorting"``
     :param str author: Name of an author.
     :param str freetext: any of the parameters, for example: ``"digibok AND Ibsen"``.
@@ -695,6 +697,9 @@ def document_corpus(
              "lang": lang, "limit": limit, "order_by": order_by}
 
     params = {x: parms[x] for x in parms if parms[x] is not None}
+
+    if not any(params.values()):
+        raise ValueError("`document_corpus` needs at least one filter argument.")
 
     r = api_post(BASE_URL + "/build_corpus", json=params)
 
